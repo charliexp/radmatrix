25572a5db91f49c48c6f9a128b5c0efd4455690e
radex <radex@hackerspace.pl>
2024-12-07 23:02:34 +0100

    increase fps to 60

--- firmware/scripts/convert.sh
+++ firmware/scripts/convert.sh
@@ -9,7 +9,7 @@ rm -fr gfx
 rm -fr video_output
 rm -fr audio
 mkdir gfx
-ffmpeg -i "$1" -vf "fps=30,scale=$scale:force_original_aspect_ratio=increase,crop=$scale,format=gray" gfx/frame_%04d.png
+ffmpeg -i "$1" -vf "fps=60,scale=$scale:force_original_aspect_ratio=increase,crop=$scale,format=gray" gfx/frame_%04d.png
 python3 scripts/gfx_to_blob.py
 mkdir audio
 ffmpeg -i "$1" -ar 44000 audio/output.wav
